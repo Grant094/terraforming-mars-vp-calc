112bd9333887659527ce68d93c036c1d9b815764
Grant Oxer <grant.oxer@gmail.com>
2023-10-10 14:47:26 -0700

    enabled calculating VPs from everything except cities with adjacent greeneries

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ function calcVictoryPoints() {
     const MAX_INT = 999; // placeholder maximum integer value
     const MAX_MILESTONES = 3; // a player cannot claim more than 3 milestones
     const MAX_ADJACENT_GREENERIES = 6; // a city cannot have more than 6 greeneries adjacent to it
-    let totalVictoryPoints = 0;
 
     let pointsByColor = new Map();
     pointsByColor.set("black", 0);
@@ -17,49 +16,57 @@ function calcVictoryPoints() {
 
     allNumbersToIntegers();
 
-    // const terraform_rating = document.getElementById("terraform_rating");
-    // adjustNumberToWithinRange(terraform_rating, MIN_INT_BESIDES_FOR_CARDS, MAX_INT);
-    // totalVictoryPoints += Number(terraform_rating.value);
+    for (let [key, value] of pointsByColor) {
+        // let elements = document.querySelectorAll('[id*=' + key + ']'); // keeping for reference should delete later
+        let totalVictoryPoints = 0;
+        let terraform_rating = document.getElementById("terraform_rating_" + key);
+        console.log(terraform_rating);
+        console.log(terraform_rating.value);
+        adjustNumberToWithinRange(terraform_rating, MIN_INT_BESIDES_FOR_CARDS, MAX_INT);
+        totalVictoryPoints += Number(terraform_rating.value);
 
-    // if (document.getElementById("winner1").checked) {
-    //     totalVictoryPoints += FIRST_PLACE_AWARD_VP;
-    // } else if (document.getElementById("contender1").checked) {
-    //     totalVictoryPoints += SECOND_PLACE_AWARD_VP;
-    // }
+        if (document.getElementById("winner1" + "_" + key).checked) {
+            totalVictoryPoints += FIRST_PLACE_AWARD_VP;
+        } else if (document.getElementById("contender1" + "_" + key).checked) {
+            totalVictoryPoints += SECOND_PLACE_AWARD_VP;
+        }
 
-    // if (document.getElementById("winner2").checked) {
-    //     totalVictoryPoints += FIRST_PLACE_AWARD_VP;
-    // } else if (document.getElementById("contender2").checked) {
-    //     totalVictoryPoints += SECOND_PLACE_AWARD_VP;
-    // }
+        if (document.getElementById("winner2" + "_" + key).checked) {
+            totalVictoryPoints += FIRST_PLACE_AWARD_VP;
+        } else if (document.getElementById("contender2_" + key).checked) {
+            totalVictoryPoints += SECOND_PLACE_AWARD_VP;
+        }
 
-    // if (document.getElementById("winner3").checked) {
-    //     totalVictoryPoints += FIRST_PLACE_AWARD_VP;
-    // } else if (document.getElementById("contender3").checked) {
-    //     totalVictoryPoints += SECOND_PLACE_AWARD_VP;
-    // }
+        if (document.getElementById("winner3" + "_" + key).checked) {
+            totalVictoryPoints += FIRST_PLACE_AWARD_VP;
+        } else if (document.getElementById("contender3" + "_" + key).checked) {
+            totalVictoryPoints += SECOND_PLACE_AWARD_VP;
+        }
 
-    // const milestonesClaimed = document.getElementById("milestones");
-    // adjustNumberToWithinRange(milestonesClaimed, MIN_INT_BESIDES_FOR_CARDS, MAX_MILESTONES);
-    // totalVictoryPoints += (Number(milestonesClaimed.value) * MILESTONE_VICTORY_POINTS);
+        const milestonesClaimed = document.getElementById("milestones" + "_" + key);
+        adjustNumberToWithinRange(milestonesClaimed, MIN_INT_BESIDES_FOR_CARDS, MAX_MILESTONES);
+        totalVictoryPoints += (Number(milestonesClaimed.value) * MILESTONE_VICTORY_POINTS);
 
-    // const greeneries = document.getElementById("greeneries");
-    // adjustNumberToWithinRange(greeneries, MIN_INT_BESIDES_FOR_CARDS, MAX_INT);
-    // totalVictoryPoints += Number(greeneries.value);
+        const greeneries = document.getElementById("greeneries" + "_" + key);
+        adjustNumberToWithinRange(greeneries, MIN_INT_BESIDES_FOR_CARDS, MAX_INT);
+        totalVictoryPoints += Number(greeneries.value);
 
-    // const citiesChildren = document.getElementById('cities').children;
-    // for (i = 0; i < citiesChildren.length; i++) {
-    //     const citiesChild = citiesChildren[i];
-    //     if (citiesChild.tagName.toString().toLowerCase() === "input") {
-    //         adjustNumberToWithinRange(citiesChild, MIN_INT_BESIDES_FOR_CARDS, MAX_ADJACENT_GREENERIES);
-    //         totalVictoryPoints += Number(citiesChild.value);
-    //     }
-    // }
+        // const citiesChildren = document.getElementById('cities').children;
+        // for (i = 0; i < citiesChildren.length; i++) {
+        //     const citiesChild = citiesChildren[i];
+        //     if (citiesChild.tagName.toString().toLowerCase() === "input") {
+        //         adjustNumberToWithinRange(citiesChild, MIN_INT_BESIDES_FOR_CARDS, MAX_ADJACENT_GREENERIES);
+        //         totalVictoryPoints += Number(citiesChild.value);
+        //     }
+        // }
 
-    // // points from cards can be anywhere from -inf to +inf, so the number does not need to be validated within a range
-    // totalVictoryPoints += Number(document.getElementById("cards").value);
+        // points from cards can be anywhere from -inf to +inf, so the number does not need to be validated within a range
+        totalVictoryPoints += Number(document.getElementById("cards" + "_" + key).value);
 
-    // document.getElementById("total_victory_points").innerHTML = "Total Victory Points: " + totalVictoryPoints.toString();
+        document.getElementById("victory_points" + "_" + key).innerHTML = totalVictoryPoints.toString();
+    }
+
+    
 };
 
 function clearAwardSelection(awardToClear) {
