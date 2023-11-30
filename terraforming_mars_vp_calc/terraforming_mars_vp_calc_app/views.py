817ad13d7e3a9e405af1c6ea96e70b9fbc335435
Grant Oxer <grant.oxer@gmail.com>
2023-11-30 13:58:05 -0800

    progress

--- terraforming_mars_vp_calc/terraforming_mars_vp_calc_app/views.py
+++ terraforming_mars_vp_calc/terraforming_mars_vp_calc_app/views.py
@@ -1,18 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.template import loader
 from .models import *
 
 # Create your views here.
 def index(request):
-    latest_game = Game.objects.order_by("timestamp")[0]
+    latest_game = Game.objects.order_by("timestamp")[:1]
     context = {
         "latest_game": latest_game,
     }
     return render(request, "terraforming_mars_vp_calc_app/index.html", context)
 
 def game(request, game_id):
-    return HttpResponse("You're looking at game %s." % game_id)
+    game = get_object_or_404(Game, pk=game_id)
+    context = {
+        "game": game,
+    }
+    return render(request, "terraforming_mars_vp_calc_app/game.html", context)
+    # return HttpResponse("You're looking at game %s." % game_id)
 
 def person(request, person_id):
     return HttpResponse("You're looking at person %s." % person_id)
