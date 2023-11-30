1ae1eebd02b0b09ef958a36ba8fbd82620701cd5
Grant Oxer <grant.oxer@gmail.com>
2023-11-30 11:33:16 -0800

    created django app

---
+++ terraforming_mars_vp_calc/terraforming_mars_vp_calc_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TerraformingMarsVpCalcAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'terraforming_mars_vp_calc_app'
