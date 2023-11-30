0005a028e604c8cfebfa201cc046abdf4b2202da
Grant Oxer <grant.oxer@gmail.com>
2023-11-30 11:38:21 -0800

    created index view and added urls.py to django app

--- terraforming_mars_vp_calc/terraforming_mars_vp_calc/urls.py
+++ terraforming_mars_vp_calc/terraforming_mars_vp_calc/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path('terraforming_mars_vp_calc_app', include("terraforming_mars_vp_calc_app.urls")),
     path('admin/', admin.site.urls),
 ]
