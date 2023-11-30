24236c32a568eb0ba38e71a509266df9f12ba0e5
Grant Oxer <grant.oxer@gmail.com>
2023-11-30 12:06:08 -0800

    created Game and Player models

--- terraforming_mars_vp_calc/terraforming_mars_vp_calc_app/models.py
+++ terraforming_mars_vp_calc/terraforming_mars_vp_calc_app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Game(models.Model):
+    timestamp = models.DateTimeField()
+
+class Player(models.Model):
+    BLACK = "BLA"
+    BLUE = "BLU"
+    GREEN = "GRE"
+    RED = "RED"
+    YELLOW = "YEL"
+    COLOR_CHOICES = [
+        (BLACK, "Black"),
+        (BLUE, "Blue"),
+        (GREEN, "Green"),
+        (RED, "Red"),
+        (YELLOW, "Yellow"),
+    ]
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    name = models.TextField(max_length=20)
+    color = models.CharField(
+        max_length=3,
+        choices=COLOR_CHOICES,
+        default=BLACK,
+    )
+    victory_points = models.IntegerField(default=20)
+    megacredits = models.IntegerField(default=0)
+    was_winner = models.BooleanField(default=False)
